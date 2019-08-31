5a04f022a4468b7c9aa95bad855adfa23528f596
Luis Fernando <lsh.fernandors@gmail.com>
2019-08-30 21:23:00 -0300

    adding map component

--- mobile/src/pages/Gym/index.js
+++ mobile/src/pages/Gym/index.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
-
+import { StyleSheet, Text } from 'react-native'
 import { 
     Container,
-    Avatar
+    Avatar,
+    GymContainer,
+    MapContainer
 } from './styles'
+
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+
 
 export default function Gym({ navigation })  {
     const [gym, setGym] = useState(null)
@@ -11,7 +16,10 @@ export default function Gym({ navigation })  {
 
     function loadGym() {
         const gym = navigation.getParam('gym')
-        console.log(gym)
+        
+        gym.location.latitudeDelta = 0.006
+        gym.location.longitudeDelta = 0.006
+
         setGym(gym)
     }
 
@@ -21,11 +29,33 @@ export default function Gym({ navigation })  {
 
     
     return (
+        // (
+        //     gym && 
+        //     <Container>
+        //         <Avatar source={{ uri: gym.logo }} />
+        //     </Container>
+        // )
         (
-            gym && 
-            <Container>
-                <Avatar source={{ uri: gym.logo }} />
-            </Container>
+            
+            gym &&
+                <Container>
+                    <GymContainer>
+                        <Text>gym</Text>
+                    </GymContainer>
+                    <MapContainer>
+
+                        <MapView
+                        style={styles.map}
+                        provider={PROVIDER_GOOGLE}
+                        showsUserLocation={true}
+                        loadingEnabled={true}
+                        initialRegion={gym.location}
+                        ref={ref => {
+                        this.mapView = ref;
+                        }}
+                        ></MapView>
+                    </MapContainer>
+                </Container>
         )
         
     )
@@ -35,3 +65,16 @@ export default function Gym({ navigation })  {
 Gym.navigationOptions = {
     title: 'Academia'
 }
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1
+    }
+});
--- mobile/src/pages/Gym/styles.js
+++ mobile/src/pages/Gym/styles.js
@@ -3,7 +3,7 @@ import { RectButton } from 'react-native-gesture-handler'
 
 export const Container = styled.View`
     flex: 1
-    align-items: center
+    
 `
 
 export const Avatar = styled.Image`
@@ -11,4 +11,12 @@ export const Avatar = styled.Image`
     height: 120px
     border-radius: 60px
     background: #eee
+`
+
+export const GymContainer = styled.View`
+    flex: 4
+`
+
+export const MapContainer = styled.View`
+    flex: 2
 `
